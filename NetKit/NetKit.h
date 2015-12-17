fdace1403701d51878abf0a71bd04d90fb868b83
Marc Palluat de Besset <marc.palluatdebesset@hibu.com>
2015-12-17 11:48:47 +0000

    Initial Commit

---
+++ NetKit/NetKit.h
@@ -0,0 +1,19 @@
+//
+//  NetKit.h
+//  NetKit
+//
+//  Created by Marc Palluat de Besset on 17/12/2015.
+//  Copyright © 2015 hibu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetKit.
+FOUNDATION_EXPORT double NetKitVersionNumber;
+
+//! Project version string for NetKit.
+FOUNDATION_EXPORT const unsigned char NetKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetKit/PublicHeader.h>
+
+
